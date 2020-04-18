3312815721d444d9df5a1821083b20243eba6767
Sang Yeop Han <piegeek@Sangs-MacBook-Pro.local>
2020-04-18 21:44:40 +0900

    Changed to rdbms

--- SnuScraper/__init__.py
+++ SnuScraper/__init__.py
@@ -1,8 +1,9 @@
 import json
 import logging
+import psycopg2
 from os.path import abspath, join, dirname
 
-from pymongo import MongoClient
+# from pymongo import MongoClient
 
 config = dict()
 
@@ -13,15 +14,28 @@ with open('SnuScraper.cfg', 'r+', encoding='utf-8') as config_file:
     config['SITE_URL'] = config_data['SITE_URL']
     config['EXCEL_URL'] = config_data['EXCEL_URL']
     config['LOG_FILE_PATH'] = abspath(join(dirname(__file__), '../log'))
+    config['DB_USER'] = config_data['DB_USER']
+    config['DB_PASSWORD'] = config_data['DB_PASSWORD']
+    config['DB_HOST'] = config_data['DB_HOST']
+    config['DB_PORT'] = config_data['DB_PORT']
+    config['DB_NAME'] = config_data['DB_NAME']
     
     with open(config_data['PARAMS_FILE'], 'r+', encoding='utf-8') as params:
         config['PARAMS'] = json.loads(params.read())
 
 
-client = MongoClient(config['CONNECTION_STRING'])
+# client = MongoClient(config['CONNECTION_STRING'])
 
-logger = logging.getLogger('SnuScraper')
-hdlr = logging.FileHandler(join(config['LOG_FILE_PATH'], 'snuscraper.log'))
+client = psycopg2.connect(
+    user=config['DB_USER'],
+    password=config['DB_PASSWORD'],
+    host=config['DB_HOST'],
+    port=config['DB_PORT'],
+    database=config['DB_NAME']
+)
+
+logger = logging.getLogger('SnuScraper') 
+hdlr = logging.FileHandler(join(config['LOG_FILE_PATH'], 'snuscraper.log')) 
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
 logger.addHandler(hdlr)
--- SnuScraper/scraper.py
+++ SnuScraper/scraper.py
@@ -63,6 +63,18 @@ class SnuScraper(object):
         else:
             raise ValueError('Parameter \'time_interval\' of function \'set_time_interval\' must be between a value of 0 and 20')
     
+    def get_appropriate_season(self):
+        if self.season == '1학기':
+            return 'SPRING'
+        elif self.season == '여름학기':
+            return 'SUMMER'
+        elif self.season == '2학기':
+            return 'AUTUMN'
+        elif self.season == '겨울학기':
+            return 'WINTER'
+        else:
+            raise ValueError('Season value not in values of ["1학기", "여름학기", "2학기", "겨울학기"]')
+
     def get_spread_sheet(self):
         '''
         Make a post request to the server with adequate parameters 
@@ -114,25 +126,118 @@ class SnuScraper(object):
             if self.old_students == True:
                 if re.search(r'(\s*)(.?\d+)(\s*)(\((\s*)(.?\d+)(\s*)\))(\s*)', lecture['정원']):
                     # TODO: Use regex later
-                    lecture['isFull'] = int(lecture['정원'].split(' ')[-1][1:-1]) <= int(lecture['수강신청인원'])
+                    lecture['is_full'] = int(lecture['정원'].split(' ')[-1][1:-1]) <= int(lecture['수강신청인원'])
                 else:
-                    lecture['isFull'] = int(lecture['정원']) <= int(lecture['수강신청인원'])
+                    lecture['is_full'] = int(lecture['정원']) <= int(lecture['수강신청인원'])
             else:
-                lecture['isFull'] = int(lecture['정원'].split(' ')[0]) <= int(lecture['수강신청인원'])
+                lecture['is_full'] = int(lecture['정원'].split(' ')[0]) <= int(lecture['수강신청인원'])
             lecture['users'] = []
             lectures.append(lecture)
 
         return lectures
     
+    def insert_lecture(self, cursor, lecture_info):
+        insert_query_text = '''INSERT INTO lectures (
+            year,
+            season, 
+            교과구분, 
+            개설대학, 
+            개설학과, 
+            이수과정, 
+            학년, 
+            교과목번호, 
+            강좌번호, 
+            교과목명, 
+            부제명, 
+            학점, 
+            강의, 
+            실습, 
+            수업교시, 
+            수업형태, 
+            강의실, 
+            주담당교수, 
+            정원, 
+            수강신청인원, 
+            비고, 
+            강의언어, 
+            개설상태, 
+            is_full
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);'''
+        
+        insert_data = (
+            int(self.year), 
+            self.get_appropriate_season(), 
+            lecture['교과구분'],
+            lecture['개설대학'],
+            lecture['개설학과'],
+            lecture['이수과정'],
+            lecture['학년'],
+            lecture['교과목번호'],
+            lecture['강좌번호'],
+            lecture['교과목명'],
+            lecture['부제명'],
+            lecture['학점'],
+            lecture['강의'],
+            lecture['실습'],
+            lecture['수업교시'],
+            lecture['수업형태'],
+            lecture['강의실(동-호)(#연건, *평창)'],
+            lecture['주담당교수'],
+            lecture['정원'],
+            lecture['수강신청인원'],
+            lecture['비고'],
+            lecture['강의언어'],
+            lecture['개설상태'],
+            lecture['is_full']
+        )
+
+        cursor.execute(insert_query_text, insert_data)
+
+    def parse_lecture_info_to_dict(self, lecture_info):
+        return {
+            'id': lecture_info[0],
+            'year': lecture_info[1],
+            'season': lecture_info[2],
+            '교과구분': lecture_info[3],
+            '개설대학': lecture_info[4],
+            '개설학과': lecture_info[5],
+            '이수과정': lecture_info[6],
+            '학년': lecture_info[7],
+            '교과목번호': lecture_info[8],
+            '강좌번호': lecture_info[9],
+            '교과목명': lecture_info[10],
+            '부제명': lecture_info[11],
+            '학점': lecture_info[12],
+            '강의': lecture_info[13],
+            '실습': lecture_info[14],
+            '수업교시': lecture_info[15],
+            '수업형태': lecture_info[16],
+            '강의실': lecture_info[17],
+            '주담당교수': lecture_info[18],
+            '정원': lecture_info[19],
+            '수강신청인원': lecture_info[20],
+            '비고': lecture_info[21],
+            '강의언어': lecture_info[22],
+            '개설상태': lecture_info[23],
+            'is_full': lecture_info[24]
+        }
+
     def save_df_to_db(self, df):
         '''
         Save data in dataframe to database
         '''
-        self.log_message('Saving spreadsheet to database(INITIALIZING DATABASE)', 'info')
+        self.log_message('Saving dataframe to database(INITIALIZING DATABASE)', 'info')
         
+        cursor = self.db.cursor()
         lectures = self.get_lecture_list(df)
+
         for lecture in lectures:
-            self.db.lectures.insert_one(lecture)
+            self.insert_lecture(cursor, lecture_info)
+
+        try:
+            self.db.commit()
+        except:
+            self.log_message('Can\'t commit to database. Rolling back...', 'error')
 
     def update_df_to_db(self, df):
         '''
@@ -141,21 +246,27 @@ class SnuScraper(object):
 
         self.log_message('Updating spreadsheet to database', 'info')
 
+        cursor = self.db.cursor()
         lectures = self.get_lecture_list(df)
 
         for lecture in lectures:
             lecture_code = lecture['교과목번호']
             lecture_number = int(lecture['강좌번호'])
 
-            lecture_cursor = self.db.lectures.find_one({
-                '$and': [
-                    { '교과목번호': lecture_code },
-                    { '강좌번호': int(lecture_number) }
-                ]
-            })
+            search_query_text = 'SELECT * FROM lectures WHERE 교과목번호 = %s AND 강좌번호 = %s LIMIT 1'
+            query_data = (lecture_code, lecture_number)
 
-            if lecture_cursor == None:
-                self.db.lectures.insert_one(lecture)
+            cursor.execute(search_query_text, query_data)
+            result = cursor.fetchall()[0]
+
+            if result == None:
+                self.insert_lecture(cursor, lecture)
+                cursor.execute(insert_query_text, insert_data)
+
+        try:
+            self.db.commit()
+        except:
+            self.log_message('Can\'t commit to database. Rolling back...', 'error')
 
            
     def get_page_student_data(self, page_num):
@@ -235,14 +346,34 @@ class SnuScraper(object):
         return updated_student_data
     
     def send_messages(self, lecture):
+        # TODO: Test this function after adding devices to the database
+
         if self.debug == True:
             return
         
         self.log_message('Sending messages', 'info')
-        users = lecture['users']
+        # users = lecture['users']
+        
+        cursor = self.db.cursor()
+        
+        id = lecture['id']
         lecture_title = lecture['교과목명']
         user_counter = 0
-        
+
+        search_query_text = '''
+        SELECT devices.device_token FROM lectures 
+        INNER JOIN registered 
+        ON registered.lecture_id = lectures.id
+        INNER JOIN users
+        ON registered.user_id = users.id
+        INNER JOIN devices
+        ON users.id = devices.user_id
+        WHERE lectures.id = %s;
+        '''
+
+        cursor.execute(search_query_text, (id))
+        users = [token_data[0] for token_data in cursor.fetchall()]
+
         # Send FCM messages
         for user in users:
             user_token = user
@@ -279,67 +410,65 @@ class SnuScraper(object):
 
         return max_student_num
     
-    def update_db(self, debug_data = None):
+    def update_db(self):
         '''
         Update student number for each lecture in the database
         '''
-        
-        if self.debug == True and debug_data != None:
-            updated_nums = debug_data
-        elif self.debug == True and debug_data == None:
-            updated_nums = self.get_student_data_async()
-        else:
-            updated_nums = self.get_student_data_async()
 
         self.log_message('updating database with scraped data', 'info')
+        updated_nums = self.get_student_data_async()
 
         updated_nums_data = updated_nums
         messaging_threads = []
+        cursor = self.db.cursor()
 
         for data in updated_nums_data:
             updated_max_num = data['정원']
             updated_num = data['수강신청인원']
 
-            # Query database with SUPER KEY('교과목번호' & '강좌번호')
-            lecture = self.db.lectures.find_one({
-                '$and': [
-                    { '교과목번호': data['교과목번호'] },
-                    { '강좌번호': int(data['강좌번호']) }
-                ]
-            })
+            search_query_text = 'SELECT * FROM lectures WHERE 교과목번호 = %s AND 강좌번호 = %s LIMIT 1;'
+            search_data = (data['교과목번호'], int(data['강좌번호']))
+
+            cursor.execute(search_query_text, search_data)
+            lecture = self.parse_lecture_info_to_dict(cursor.fetchall()[0])
 
             # When there is a new lecture that doesn't exist in the database
             if lecture == None:
                 continue
-            
-            id = lecture['_id']
 
             max_student_num = self.extract_max_student_number(lecture['정원'])
             updated_max_student_num = self.extract_max_student_number(updated_max_num)
 
-            is_full = lecture['isFull']
-            query = { '_id': ObjectId(id) }
+            id = lecture['id']
+            is_full = lecture['is_full']
 
             if updated_num < updated_max_student_num and is_full == True:
                 self.log_message(f'1, title: {lecture["교과목명"]}, updated_num: {updated_num}, max_student_num: {updated_max_student_num}', 'info')
-                new_values = {'$set': { '수강신청인원': updated_num, 'isFull': False, '정원': updated_max_num }}
+
+                update_query_text = 'UPDATE lectures SET 수강신청인원 = %s, is_full = %s, 정원 = %s WHERE id = %s;'
+                update_query_data = (updated_num, False, updated_max_num, id)
                 
                 messaging_thread = threading.Thread(target=self.send_messages, args=(lecture,))
                 messaging_threads.append(messaging_thread)
                 messaging_thread.start()            
             
             elif updated_num >= updated_max_student_num and is_full == False:
-               self.log_message(f'2, title: {lecture["교과목명"]}, updated_num: {updated_num}, max_student_num: {updated_max_student_num}', 'info')
-               new_values = {'$set': { '수강신청인원': updated_num, 'isFull': True, '정원': updated_max_num }}
+                self.log_message(f'2, title: {lecture["교과목명"]}, updated_num: {updated_num}, max_student_num: {updated_max_student_num}', 'info')
+                
+                update_query_text = 'UPDATE lectures SET 수강신청인원 = %s, is_full = %s, 정원 = %s WHERE id = %s;'
+                update_query_data = (updated_num, True, updated_max_num, id) 
             
             else:
-                new_values = {'$set': { '수강신청인원': updated_num, '정원': updated_max_num } }
+                update_query_text = 'UPDATE lectures SET 수강신청인원 = %s, 정원 = %s WHERE id = %s;'
+                update_query_data = (updated_num, updated_max_num, id)
 
             # Update database
-            try:
-                self.db.lectures.update_one(query, new_values)
-            except ValueError:
-                continue
+            cursor.execute(update_query_text, update_query_data)
+
+        try:
+            self.db.commit()
+        except:
+            self.log_message('Can\'t commit to database. Rolling back...', 'error')
 
         for messaging_thread in messaging_threads:
             if messaging_thread:
